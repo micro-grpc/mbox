6183df20f891864d01a30801d39bbb5956212e06
Oleg Dolya <oleg.dolya@gmail.com>
2018-08-28 11:32:59 +0300

    add to lib ReplaceNameToKey

--- lib/utils.go
+++ lib/utils.go
@@ -2,6 +2,9 @@ package lib
 
 import (
   "fmt"
+  "regexp"
+  "strings"
+  "bytes"
 )
 // FilteringMode - return sql operator
 func FilteringMode(src string, key int) string {
@@ -25,4 +28,18 @@ func FilteringMode(src string, key int) string {
   default:
     return "!=0"
   }
+}
+
+// ReplaceNameToKey - преобразуем строку в ключ для CouchDB
+// src := "Привет|name? \\ / {} ! `@#$%^&()-+=~ '<[ витя \"рога Копїта \"]>' = 1.2кг 1,03"
+// stopWord := " кг | для | литр | і | и | нет"
+// @response - привет_name_витя_рога_копїта
+func ReplaceNameToKey(src string, stopWord string) (result string) {
+  r, _ := regexp.Compile("[0-9,.\\]\\[<>|'?!`\\\\@#$%^&()\\-/{}+=~\"]+")
+  res := r.ReplaceAllString(strings.ToLower(src), " ")
+  r = regexp.MustCompile(fmt.Sprintf("(%s)+", stopWord))
+  res = r.ReplaceAllString(res, " ")
+  re := regexp.MustCompile("  +")
+  replaced := re.ReplaceAll(bytes.TrimSpace([]byte(res)), []byte(" "))
+  return strings.Replace(string(replaced), " ", "_", -1)
 }
